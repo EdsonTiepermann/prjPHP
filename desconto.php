42356588dbcaebce2b6b3ee792a874df0e5314d6
tiepermann59 <tiepermann59@gmail.com>
2020-02-23 20:26:42 -0300

    Funçoes de desconto

--- desconto.php
+++ desconto.php
@@ -1,9 +1,13 @@
 <?php
-    function calcula_desconto()
-    {
-        $valor_desconto = $valor_total * ($desconto/100);
-        $valor_total_com_desconto = $valor_total - $valor_desconto;
 
-        return $valor_total_com_desconto;
-    }
+require_once("funcoes_desconto.php");
+
+$valor_total = 800;
+$desconto = 10;
+
+$valor_total_com_desconto = calcula_desconto($valor_total, $desconto);
 ?>
+
+    Valor total: R$ <?php echo $valor_total ?> <br>
+    Valor desconto R$ <?php echo $desconto ?>% <br>
+    Valor total com desconto: R$ <?php $valor_total_com_desconto ?>
--- funcoes_desconto.php
+++ funcoes_desconto.php
@@ -1,13 +1,10 @@
 <?php
+function calcula_desconto($valor_total, $desconto)
+{
+    $valor_desconto = $valor_total * ($desconto / 100);
+    $valor_total_com_desconto = $valor_total - $valor_desconto;
 
-    require_once ("funcoes_desconto.php");
+    return $valor_total_com_desconto;
+}
 
-    $valor_total = 800;
-    $desconto = 10;
-
-    $valor_total_com_desconto = calcula_desconto($valor_total, $desconto);
 ?>
-
-Valor total: R$ <?php echo $valor_total ?> <br>
-Valor desconto R$ <?php echo $desconto ?>% <br>
-Valor total com desconto: R$ <?php $valor_total_com_desconto ?>
