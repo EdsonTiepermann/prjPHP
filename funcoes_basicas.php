480bbd167a6ad4613ef661618dfbebb672856d1f
EdsonTiepermann <tiepermann59@gmail.com>
2020-02-27 00:29:27 -0300

    no message

--- funcoes_basicas.php
+++ funcoes_basicas.php
@@ -6,7 +6,12 @@
 
     // retorna true quando = '',0, '0', false, null, array()
     // false = outras condições
-
     $valor = '';
     empty($valor);
+
+    //verifica se a variável é um número
+    $valor = 0;
+    is_numeric($valor);
+
+
 ?>
