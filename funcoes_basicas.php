0fd3ea230aac90a94534bd50a9e38a7a341815c4
EdsonTiepermann <tiepermann59@gmail.com>
2020-02-27 00:28:04 -0300

    no message

---
+++ funcoes_basicas.php
@@ -0,0 +1,12 @@
+<?php
+
+    //verifica se a variável está iniciada
+    $valor = "edson";
+    isset($valor);
+
+    // retorna true quando = '',0, '0', false, null, array()
+    // false = outras condições
+
+    $valor = '';
+    empty($valor);
+?>
